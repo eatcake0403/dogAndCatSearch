e1ef74a459a54685f97c1a4dd1c9803dcaea5fad
wewe.chiu <wewe.chiu@1177tech.com.tw>
2022-06-20 17:46:34 +0800

    add: cat

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 const origFetch = window.fetch
 window.fetch = async (...args) => {
   try {
+    document.querySelector('#spin').style.display = 'block'
     return await origFetch(...args)
   } catch (err) {
     window.alert(err.message)
     throw err
+  } finally {
+    document.querySelector('#spin').style.display = 'none'
   }
 }
 
-const baseURL = 'https://dog.ceo/api'
+const baseURLDog = 'https://dog.ceo/api'
+const baseURLCat = 'https://cataas.com'
 
 async function getBreeds () {
-  const reqURL = `${baseURL}/breeds/list/all`
+  const reqURL = `${baseURLDog}/breeds/list/all`
   return (await (await fetch(reqURL)).json()).message
 }
-
 
 async function fillBreeds (vals) {
   const element = document.querySelector('#breedsSelect')
@@ -26,9 +29,8 @@ async function fillBreeds (vals) {
   }
 }
 
-
 async function searchImg (breed) {
-  const reqURL = `${baseURL}/breed/${breed}/images/random`
+  const reqURL = `${baseURLDog}/breed/${breed}/images/random`
   return (await (await fetch(reqURL)).json()).message
 }
 
@@ -37,6 +39,40 @@ async function fillImg (breed) {
   const element = document.querySelector('#dogImg')
   const img = await searchImg(breed)
   element.src = img
+}
+
+function changeTab (tab) {
+  const elements = document.querySelector('#tabs').children
+  const cards = document.querySelectorAll('.card')
+  for(let i = 0; i < elements.length; i++) {
+    elements[i].classList.remove('active')
+  }
+  elements[tab].classList.add('active')
+  for(let i = 0; i < cards.length; i++) {
+    cards[i].classList.add('none')
+  }
+  cards[tab].classList.remove('none')
+}
+
+async function randomImgCat () {
+  const randomNum = Math.floor(Math.random()*10000)
+  const reqURL = `${baseURLCat}/cat?randomNum=${randomNum}`
+  const img = URL.createObjectURL(await (await fetch(reqURL)).blob())
+  document.querySelector('#CatImg').src = img
+}
+
+async function randomGIFCat () {
+  const randomNum = Math.floor(Math.random()*10000)
+  const reqURL = `${baseURLCat}/cat/gif?randomNum=${randomNum}`
+  const img = URL.createObjectURL(await (await fetch(reqURL)).blob())
+  document.querySelector('#CatImg').src = img
+}
+
+async function sayTextCat (text) {
+  const randomNum = Math.floor(Math.random()*10000)
+  const reqURL = `${baseURLCat}/cat/says/${encodeURI(text)}?randomNum=${randomNum}`
+  const img = URL.createObjectURL(await (await fetch(reqURL)).blob())
+  document.querySelector('#CatImg').src = img
 }
 
 window.onload = async () =>  {
